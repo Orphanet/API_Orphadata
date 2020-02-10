e2e94176738e1b718354bfad024790d00d82859c
cybigot <cyril.bigot.pro@gmail.com>
2020-02-10 11:02:00 +0100

    First commit

---
+++ python-flask-server/swagger_server/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.orpha_number import OrphaNumber
+from swagger_server.models.user import User
---
+++ python-flask-server/swagger_server/test/test_orpha_number_controller.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.orpha_number import OrphaNumber  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestOrphaNumberController(BaseTestCase):
+    """OrphaNumberController integration test stubs"""
+
+    def test_get_orpha_number_by_id(self):
+        """Test case for get_orpha_number_by_id
+
+        Find a disorder by ORPHAnumber
+        """
+        response = self.client.open(
+            '/v2/{OrphaNumber}'.format(OrphaNumber=56),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
