15c29d6bff7cc8b7316056e49bbc74f995248043
cybigot <cyril.bigot.pro@gmail.com>
2020-02-11 12:27:15 +0100

    Empty dir to get new server build from swagger

--- python-flask-server-v3/swagger_server/controllers/authorization_controller.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-"""
-controller generated to handled auth operation described at:
-https://connexion.readthedocs.io/en/latest/security.html
-"""
-def check_HTTP_auth(username, password, required_scopes):
-    return {'test_key': 'test_value'}
-
-
