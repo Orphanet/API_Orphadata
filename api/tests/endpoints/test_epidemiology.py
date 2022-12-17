ad78a49bb240296319ee8cae4b064cdcc4d5865d
nchenche <nicolas.chevrollier@laposte.net>
2022-12-17 15:52:55 +0100

    add tests for natural history endpoints

--- api/tests/endpoints/test_epidemiology.py
+++ api/tests/endpoints/test_epidemiology.py
@@ -67,6 +67,50 @@ class TestEpidemiologyEndpointsStatus(BaseTestCase):
         response = self.client.get(url_endpoint)
         self.assert404(response)
 
+    def test_status_200_natural_history_orphacodes(self):
+        """test_status_200_epidemiology_orphacodes
+
+        SETUP: TEST STATUS 200 for /orphadata/details/rd-natural_history/orphacodes
+            - a url endpoint
+        ACTION:
+            - GET request on valid url endpoint
+        CHECK:
+            - response status is 200
+        """
+        url_endpoint = self.URL_ENDPOINTS.get('history_orphacodes')
+        response = self.client.get(url_endpoint)
+        self.assert200(response)
+
+    def test_status_200_natural_history_by_orphacode(self):
+        """test_status_200_epidemiology_by_orphacode
+
+        SETUP: TEST STATUS 200 /orphadata/details/rd-natural_history/orphacodes/{}
+            - a url endpoint
+                - with a valid orphacode
+        ACTION:
+            - GET request on valid url endpoint
+        CHECK:
+            - response status is 200
+        """
+        url_endpoint = self.URL_ENDPOINTS.get('history_by_orphacode').format(self.valid_orphacode)
+        response = self.client.get(url_endpoint)
+        self.assert200(response)
+
+    def test_status_404_natural_history_by_orphacode(self):
+        """test_status_404_epidemiology_by_orphacode
+
+        SETUP: TEST STATUS 404 /orphadata/details/rd-natural_history/orphacodes/{}
+            - a url endpoint
+                - with an invalid orphacode
+        ACTION:
+            - GET request on valid url endpoint
+        CHECK:
+            - response status is 404
+        """
+        url_endpoint = self.URL_ENDPOINTS.get('history_by_orphacode').format(self.invalid_orphacode)
+        response = self.client.get(url_endpoint)
+        self.assert404(response)
+
 
 if __name__ == '__main__':
     import unittest
